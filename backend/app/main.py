5dfcb77069d1832010f27c6b5f196cc49f6155ec
samuelselasi <samelsekasi@gmail.com>
2023-12-09 18:39:22 +0000

    Add endpoints for regions and countries routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,11 +4,15 @@
 from fastapi import FastAPI
 from app.routers.auth import main as auth
 from app.routers.user import main as user
+from app.routers.regions import main as regions
+from app.routers.countries import main as countries
 
 app = FastAPI(debug=True)
 
 app.include_router(auth.router, tags=["Authentication"])
-app.include_router(user.router, tags=["User"])
+app.include_router(user.router, tags=["Users"])
+app.include_router(regions.router, tags=["Regions"])
+app.include_router(countries.router, tags=["Countries"])
 
 
 @app.get("/")
