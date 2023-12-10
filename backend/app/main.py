d6cfebe45c196d370f06e8eb8ffd7cb5d9f38c66
samuelselasi <samelsekasi@gmail.com>
2023-12-10 16:14:57 +0000

    Add router containing endpoints for currency

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,7 @@ from app.routers.user import main as user
 from app.routers.regions import main as regions
 from app.routers.countries import main as countries
 from app.routers.provinces import main as provinces
+from app.routers.currencies import main as currencies
 
 app = FastAPI(debug=True)
 
@@ -15,6 +16,7 @@ app.include_router(user.router, tags=["Users"])
 app.include_router(regions.router, tags=["Regions"])
 app.include_router(countries.router, tags=["Countries"])
 app.include_router(provinces.router, tags=["Provinces"])
+app.include_router(currencies.router, tags=["Currencies"])
 
 
 @app.get("/")
