038b3bd6d0f8a06f073f807e5a4f3bf468a41dbe
samuelselasi <samelsekasi@gmail.com>
2023-12-12 14:46:21 +0000

    Add module that handles db schemas for timezones

---
+++ backend/app/routers/timezones/schemas.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""Module that defines orm schemas for tables"""
+
+from typing import List
+from pydantic import BaseModel
+
+
+class CountryBase(BaseModel):
+    """Class that defines instance attributes"""
+
+    name: str
+
+
+class CountryCreate(CountryBase):
+    """Class that passes instance attributes"""
+
+    pass
+
+
+class Country(CountryBase):
+    """Class that defines instance attributes"""
+
+    id: int
+    region_id: int
+
+    class Config:
+        """Class that configures ORM mode"""
+
+        from_attributes = True
+
+
+class RegionBase(BaseModel):
+    """Class that defines instance attributes"""
+
+    name: str
+
+
+class RegionCreate(RegionBase):
+    """Class that defines instance attributes"""
+
+    name: str
+
+
+class Region(RegionBase):
+    """Class that defines instance attributes"""
+
+    id: int
+    is_active: bool
+    countries: List[Country] = []
+
+    class Config:
+        """Class that configures ORM mode"""
+
+        from_attributes = True
+
+
+class Region_(RegionBase):
+    """Class that defines instance attributes"""
+
+    id: int
+
+    class Config:
+        """Class that configures ORM mode"""
+
+        from_attributes = True
+
+
+class TimezoneBase(BaseModel):
+    """Class that defines instance attributes"""
+
+    name: str
+
+
+class TimezoneCreate(TimezoneBase):
+    """Class that passes instance attributes"""
+
+    pass
+
+
+class Timezone(TimezoneBase):
+    """Class that defines instance attributes"""
+
+    id: int
+    country: Country
+
+    class Config:
+        """Class that configures ORM mode"""
+
+        from_aTimezones = True
