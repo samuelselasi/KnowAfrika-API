f92f6c8a294810b92b86cf3dff77e4b0a9ef7492
samuelselasi <samelsekasi@gmail.com>
2023-12-12 12:55:51 +0000

    Add module that handles db schemas for transportation

---
+++ backend/app/routers/transport/schemas.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""Module that defines orm schemas for tables"""
+
+from typing import List
+from pydantic import BaseModel
+
+
+class CountryBase(BaseModel):
+    """Class that defines instance attributes"""
+
+    name: str
+
+
+class CountryCreate(CountryBase):
+    """Class that passes instance attributes"""
+
+    pass
+
+
+class Country(CountryBase):
+    """Class that defines instance attributes"""
+
+    id: int
+    region_id: int
+
+    class Config:
+        """Class that configures ORM mode"""
+
+        orm_mode = True
+
+
+class RegionBase(BaseModel):
+    """Class that defines instance attributes"""
+
+    name: str
+
+
+class RegionCreate(RegionBase):
+    """Class that defines instance attributes"""
+
+    name: str
+
+
+class Region(RegionBase):
+    """Class that defines instance attributes"""
+
+    id: int
+    is_active: bool
+    countries: List[Country] = []
+
+    class Config:
+        """Class that configures ORM mode"""
+
+        orm_mode = True
+
+
+class Region_(RegionBase):
+    """Class that defines instance attributes"""
+
+    id: int
+
+    class Config:
+        """Class that configures ORM mode"""
+
+        orm_mode = True
+
+
+class TransportBase(BaseModel):
+    """Class that defines instance attributes"""
+
+    name: str
+
+
+class TransportCreate(TransportBase):
+    """Class that passes instance attributes"""
+
+    pass
+
+
+class Transport(TransportBase):
+    """Class that defines instance attributes"""
+
+    id: int
+    country: Country
+
+    class Config:
+        """Class that configures ORM mode"""
+
+        orm_mode = True
