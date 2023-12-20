94277e1cb484400a09cce8e581fa7961bbe0a1dd
samuelselasi <samelsekasi@gmail.com>
2023-12-20 22:08:10 +0000

    Add constitutions router

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,6 +11,7 @@ from app.routers.cities import main as cities
 from app.routers.currencies import main as currencies
 from app.routers.languages import main as languages
 from app.routers.flags import main as flags
+from app.routers.constitutions import main as constitutions
 from app.routers.transport import main as transport
 from app.routers.timezones import main as timezones
 
@@ -25,6 +26,7 @@ app.include_router(cities.router, tags=["Cities"])
 app.include_router(currencies.router, tags=["Currencies"])
 app.include_router(languages.router, tags=["Languages"])
 app.include_router(flags.router, tags=["Flags"])
+app.include_router(constitutions.router, tags=["Constitutions"])
 app.include_router(transport.router, tags=["Transportation"])
 app.include_router(timezones.router, tags=["Time-zones"])
 
